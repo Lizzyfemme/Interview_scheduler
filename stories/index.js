0685de736d9376e145201c03ee3b38f0d6aa0b0d
Lizzyfemme <lizzy_swann@hotmail.com>
2019-10-29 14:45:37 -0700

    Confirmation banner added

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -139,3 +140,4 @@ storiesOf("Button", module)
         .add("Header", () => <Header time="12pm" />)
         .add("Empty", () => <Empty onAdd={action("onAdd")} />)
         .add("Show", () => <Show name="Bob" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
+        .add("Confirm", () => <Confirm string="Delete the appointment?" onConfirm={action("onConfirm")}  onCancel={action("onCancel")} />)
